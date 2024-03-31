7734db7569346214920c952ac9bfd4e758a644a9
BabaYaga-1999 <100554529+BabaYaga-1999@users.noreply.github.com>
2024-03-30 21:26:37 -0700

    Add files via upload

--- CnnTrain.py
+++ CnnTrain.py
@@ -5,6 +5,8 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
 
 # Load the data
 df = pd.read_csv('keypoints.csv')
@@ -25,19 +27,47 @@ X_test = X_test.reshape(-1, 21, 2, 1)
 
 # Build the model
 model = Sequential([
-    Conv2D(32, kernel_size=(1, 2), activation='relu', input_shape=(21, 2, 1)),  # Adjusted kernel size
-    MaxPooling2D(pool_size=(2, 1)),  # 调整了池化层的尺寸
+    Conv2D(32, kernel_size=(1, 2), activation='relu', input_shape=(21, 2, 1)),
+    MaxPooling2D(pool_size=(2, 1)),
     Flatten(),
     Dense(128, activation='relu'),
     Dropout(0.5),
     Dense(y_categorical.shape[1], activation='softmax')
 ])
 
-
 model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50)
+# Setup early stopping
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min', restore_best_weights=True)
+
+# Train the model with early stopping
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, callbacks=[early_stopping])
 
 # Save the model
 model.save('hand_gesture_model.h5')
+
+# Plotting training and validation loss
+plt.figure(figsize=(10, 5))
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('training_validation_loss.png')  # Save the loss plot as a PNG file
+plt.show()  # Show the plot
+
+# Clear the current figure to make sure the next plot doesn't overlap with the previous one
+plt.clf()
+
+# Plotting training and validation accuracy
+plt.figure(figsize=(10, 5))
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('training_validation_accuracy.png')  # Save the accuracy plot as a PNG file
+plt.show()  # Show the plot
+
